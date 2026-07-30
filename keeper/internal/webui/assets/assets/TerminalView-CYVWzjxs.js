82a8dc42ae3152a68aa21f6227c1cd4d85097ce3
co-cy <ops@soul-stack.com>
2026-07-29 23:03:38 -0300

    chore(webui): re-sync the embedded UI bundle after NIM-269

--- keeper/internal/webui/assets/assets/TerminalView-CYVWzjxs.js
+++ keeper/internal/webui/assets/assets/TerminalView-CYVWzjxs.js
@@ -1,4 +1,4 @@
-import{r as Re,j as An,s as On}from"./index-MY8TA11s.js";/**
+import{r as Re,j as An,s as On}from"./index-LSno9NQb.js";/**
  * Copyright (c) 2014-2024 The xterm.js authors. All rights reserved.
  * @license MIT
  *
