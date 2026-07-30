a80db1f3bb9193a729b83b0f8dff184e853a2bba
co-cy <ops@soul-stack.com>
2026-07-29 22:27:09 -0300

    chore(webui): re-sync the embedded UI bundle after the sidebar regrouping (NIM-322)

--- keeper/internal/webui/assets/assets/TerminalView-CB36wPaw.js
+++ keeper/internal/webui/assets/assets/TerminalView-CB36wPaw.js
@@ -1,4 +1,4 @@
-import{r as Re,j as An,s as On}from"./index-Imveh48L.js";/**
+import{r as Re,j as An,s as On}from"./index-DQGfwtIr.js";/**
  * Copyright (c) 2014-2024 The xterm.js authors. All rights reserved.
  * @license MIT
  *
